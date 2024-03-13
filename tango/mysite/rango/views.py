6bd37c094ce82522da20eb904259a31ec074894b
Robert De Venny <2892269d@student.gla.ac.uk>
2024-03-13 15:08:51 +0000

    chapter 7 - form added and working

--- tango/mysite/rango/views.py
+++ tango/mysite/rango/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -47,6 +49,17 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
 
 
 
